79e6916ec6670f318b9b7d11899cac42606fc45b
HcwXd <e04dl47878@gmail.com>
2018-10-20 14:27:07 +0800

    Add drag to rotate

--- interactive/js/index.js
+++ interactive/js/index.js
@@ -1,26 +1,27 @@
 let dataset = {};
-let opacity = 0;
 let isRepeat = true;
 let rotateX = 0;
+let rotateY = 0;
+let projectionType = 'equirectangular'; // mercator / azimuthalEqualArea / equirectangular / orthographic
 
 setInterval(() => {
   if (isRepeat) {
-    IterateDataset();
-    renderMap(dataset, 'equirectangular');
+    generateDataset();
+    renderMap(dataset);
   }
-  // isRepeat = false;
+  isRepeat = false;
 }, 100);
 
-function IterateDataset() {
+function generateDataset() {
   randomDataset();
 
-  var onlyValues = series.map(function(obj) {
+  const onlyValues = series.map(function(obj) {
     return obj[1];
   });
-  var minValue = Math.min.apply(null, onlyValues);
-  var maxValue = Math.max.apply(null, onlyValues);
+  const minValue = Math.min.apply(null, onlyValues);
+  const maxValue = Math.max.apply(null, onlyValues);
 
-  var paletteScale = d3.scale
+  const paletteScale = d3.scale
     .linear()
     .domain([minValue, maxValue])
     .range(['#EFEFFF', '#02386F']);
@@ -29,31 +30,25 @@ function IterateDataset() {
     let country = item[0];
     let value = item[1];
     dataset[country] = { numberOfThings: value, fillColor: paletteScale(value) };
-    // dataset[country] = { numberOfThings: value, fillColor: `rgba(255, 0, 0, ${opacity})` };
   });
-  opacity += 0.01;
 }
 
-function renderMap(dataset, projection) {
-  document.getElementById('container1').innerHTML = '';
-  new Datamap({
+function renderMap(dataset) {
+  const livemap = new Datamap({
     element: document.getElementById('container1'),
-    projection: projection, // mercator / azimuthalEqualArea / equirectangular / orthographic
     responsive: true,
 
     fills: { defaultFill: '#F5F5F5' },
     data: dataset,
 
     setProjection: function(element) {
-      var projection = d3.geo
-        .equirectangular()
+      const projection = d3.geo[projectionType]()
         .center([0, 0])
-        .rotate([rotateX, 0])
+        .rotate([rotateX, rotateY])
         .scale(250)
         .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
 
-      var path = d3.geo.path().projection(projection);
-      rotateX += 1;
+      const path = d3.geo.path().projection(projection);
       return { path: path, projection: projection };
     },
 
@@ -85,6 +80,36 @@ function renderMap(dataset, projection) {
       },
     },
   });
+  const drag = d3.behavior.drag().on('drag', function() {
+    var dx = d3.event.dx;
+    var dy = d3.event.dy;
+
+    var rotation = [rotateX, rotateY];
+    var radius = livemap.projection.scale();
+    var scale = d3.scale
+      .linear()
+      .domain([-1 * radius, radius])
+      .range([-90, 90]);
+    var degX = scale(dx);
+    var degY = scale(dy);
+    rotation[0] += degX;
+    rotation[1] -= degY;
+    if (rotation[1] > 90) rotation[1] = 90;
+    if (rotation[1] < -90) rotation[1] = -90;
+
+    if (rotation[0] >= 180) rotation[0] -= 360;
+    [rotateX, rotateY] = [...rotation];
+    redraw();
+  });
+
+  d3.select('#container1')
+    .select('svg')
+    .call(drag);
+}
+
+function redraw() {
+  d3.select('#container1').html('');
+  renderMap(dataset);
 }
 
 function randomDataset() {
